a770c3173427668e5123bea6caecfdeefa630e85
Taxato <taxatoftw@gmail.com>
2024-02-15 14:35:03 +0100

    screenshots and top margin

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Content from "./components/Content";
 
 export default function App() {
 	return (
-		<div className="mt-8 flex h-full w-full justify-center md:mt-32 lg:mt-48">
+		<div className="mt-12 flex  w-full justify-center md:mt-32 lg:mt-48">
 			<Content />
 		</div>
 	);
